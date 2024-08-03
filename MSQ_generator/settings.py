b0982b16a675c2c9c212ec86d1e69069b26f22c6
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-08-03 14:31:32 -0300

    Update settings env vars
    See https://www.upwork.com/jobs/Deploy-span-class-highlight-Django-span-app-Azure-Web-App_~019d9980c91573acde/?referrer_url_path=%2Fnx%2Fsearch%2Fjobs%2F

--- MSQ_generator/settings.py
+++ MSQ_generator/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nwx0gi#5&&g#8$x7vtbp-f(^@1scd+#+(rdk^2c_c0d5!qf14h'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('DEBUG') == 'True' else False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '')]
+CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS', '')]
 
 # Application definition
 
